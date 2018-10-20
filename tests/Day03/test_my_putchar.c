327b1402256982dcf7e62b4bed091cb3d14454f1
Guillaume <guillaume@localhost.localdomain>
2018-10-20 10:43:55 +0200

    Removed broken tests

--- tests/Day03/test_my_putchar.c
+++
@@ -1,9 +0,0 @@
-/*
-** EPITECH PROJECT, 2018
-** test_my_putchar
-** File description:
-** Unit tests for my_putchar
-*/
-
-#include <criterion/criterion.h>
-
--- tests/test_output.c
+++
@@ -1,7 +0,0 @@
-#include <criterion/redirect.h>
-
-
-Test(fct, name, .init = cr_redirect_stdout)
-{
-    cr_assert_stdout_eq_str();
-}
